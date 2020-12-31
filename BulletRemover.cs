ff9bae0c10c57e1c67668a81e3132a165b7dd017
the-garbage-which-belongs-to-waffle <60797216+the-garbage-which-belongs-to-waffle@users.noreply.github.com>
2020-12-31 22:33:24 +0000

    Add files via upload

---
+++ BulletRemover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using BulletRemoverMod;
+using Harmony;
+using UnityEngine;
+using NodeCanvas.Tasks.Actions;
+
+[assembly: MelonInfo(typeof(BulletRemover), "Bullet Remover", "1.0.0", "Waffle (but actually mostly october)")]
+[assembly: MelonGame("SUPERHOT_Team", "SUPERHOT_VR")]
+
+namespace BulletRemoverMod
+{
+    class BulletRemover : MelonMod
+    {
+        [HarmonyPatch(typeof(Bullet), "Update", MethodType.Normal)]
+        private static class destroyBullets {
+            private static void Postfix(Bullet  __instance) {
+                UnityEngine.Object.Destroy(__instance);
+            }
+        }
+    }
+}
